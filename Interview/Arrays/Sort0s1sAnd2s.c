f0e48f611283df9858d8572207d8f7fde6544ea9
Paras Porwal <parasporwal@QAIT.COM>
2019-03-24 22:21:33 +0530

    solving sort012 using dutch flag problem

--- Interview/Arrays/Sort0s1sAnd2s.c
+++ Interview/Arrays/Sort0s1sAnd2s.c
@@ -51,3 +51,60 @@ int main() {
 	return 0;
 }
 
+/*
+ *  Dutch Flag Problem O(n) and better approach
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void printArray(int * arr,int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+void swap(int* a, int * b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void segregate(int *arr, int size){
+    int low=0, mid=0, high=size-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(&arr[mid],&arr[low]);
+             mid++;
+             low++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(&arr[mid],&arr[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+	int t;
+	int size;
+	int * arr;
+	scanf("%d ",&t);
+	while(t--){
+	    scanf("%d ",&size);
+	    arr=(int*)malloc(sizeof(int)*size);
+	    for(int i=0;i<size;i++){
+	        scanf("%d ",&arr[i]);
+	    }
+	    segregate(arr,size);
+	    printArray(arr,size);
+	    printf("\n");
+
+	}
+	return 0;
+}
+
+
+
